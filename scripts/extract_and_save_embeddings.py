6374d21c98c118fee17ea66907f375aba39c8a22
heitor <heitor.nolla@gmail.com>
2026-04-29 06:04:19 -0300

    adjust to mcyt

--- scripts/extract_and_save_embeddings.py
+++ scripts/extract_and_save_embeddings.py
@@ -3,7 +3,7 @@ import os
 from flx.data.dataset import *
 from flx.data.embedding_loader import EmbeddingLoader
 from flx.data.image_helpers import pad_and_resize_to_deepprint_input_size
-from flx.data.image_loader import BaseNLoader
+from flx.data.image_loader import MCYTCapacitiveLoader
 from flx.data.transformed_image_loader import TransformedImageLoader
 
 from flx.extractor.fixed_length_extractor import get_DeepPrint_TexMinu, DeepPrintExtractor
@@ -12,9 +12,7 @@ from flx.image_processing.binarization import LazilyAllocatedBinarizer
 
 
 MODEL_DIR: str = os.path.abspath("models")
-DATASET_PATH: str = os.path.abspath("/storage/datasets/full/baseN_aligned/images/")
-OUT_DIRS = ['./latents/', './references']
-OUT_NAMES = ['latents.npy', 'references.npy']
+DATASET_PATH: str = os.path.abspath("/storage/datasets/full/mcyt330dp/")
 
 
 def get_extractor(model_dir):
@@ -51,6 +49,5 @@ def save_embeddings(embeddings, out_path, out_name):
 if __name__ == "__main__":
     extractor = get_extractor(MODEL_DIR)
 
-    for out_dir, i in OUT_DIRS:
-        embeddings = get_embeddings(extractor, DATASET_PATH+out_dir, BaseNLoader)
-        embeddings.save(out_dir, OUT_NAMES[i])
+    embeddings = get_embeddings(extractor, DATASET_PATH, MCYTCapacitiveLoader)
+    embeddings.save('.')
